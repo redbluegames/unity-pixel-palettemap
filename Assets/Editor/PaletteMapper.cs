5c97004686f782414936841b7f294429aa6f973b
Edward Rowe <rowedrage@gmail.com>
2014-11-13 14:29:59 -0500

    Pulled out logic from GUI code a bit.

---
+++ Assets/Editor/PaletteMapper.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class PaletteMapper {
+
+	public static void ValidateSourceTexture (Texture2D sourceTexture)
+	{
+		TextureImporter textureImporter = AssetImporter.GetAtPath(AssetDatabase.GetAssetPath(sourceTexture)) as TextureImporter;
+		if(!textureImporter.isReadable) {
+			throw new System.ArgumentException("PaletteMapper Error: Source texture must be Read/Write enabled.");
+		}
+		
+		if (sourceTexture.filterMode != FilterMode.Point) {
+			throw new System.ArgumentException ("PaletteMapper Error: Source texture must have Point filter mode.");
+		}
+		
+		if (sourceTexture.format != TextureFormat.RGBA32) {
+			throw new System.ArgumentException ("PaletteMapper Error: Source texture must be uncompressed (RGBA32)");
+		}
+	}
+
+	public static void WritePaletteMapTextureToDisk (string outputPath, Texture2D inTexture, bool overwriteExistingFiles)
+	{
+		// Create full path to output file
+		string paletteMapSuffix = "_PaletteMap.png";
+		string filename = inTexture.name + paletteMapSuffix;
+		string fullPathToOutputFile = outputPath + filename;
+		
+		// Handle file overwriting
+		if(File.Exists(fullPathToOutputFile)) {
+			if(!overwriteExistingFiles) {
+				Debug.LogError("PaletteMap Error: Tried to write " + filename + " but file already exists. " +
+				               "\nFile Path: " + outputPath);
+				return;
+			}
+			Debug.LogWarning("PaletteMap: Overwriting file " + filename);
+		}
+		
+		// Create the PaletteMap texture
+		Texture2D outTexture = CreatePaletteMapFromSource(inTexture);
+		
+		// Write the PaletteMap to disk
+		try {
+			byte[] outTextureData = outTexture.EncodeToPNG ();
+			File.WriteAllBytes (fullPathToOutputFile, outTextureData);
+			
+			// Assign correct settings to the file
+			TextureImporter textureImporter = AssetImporter.GetAtPath(fullPathToOutputFile) as TextureImporter; 
+			textureImporter.filterMode = FilterMode.Point;
+			textureImporter.textureFormat = TextureImporterFormat.Alpha8;
+			textureImporter.alphaIsTransparency = true;
+			textureImporter.mipmapEnabled = false;
+			textureImporter.npotScale = TextureImporterNPOTScale.None;
+			
+			// Force Unity to see the file and use the new import settings
+			AssetDatabase.ImportAsset(fullPathToOutputFile); 
+			
+			Debug.Log ("<color=green>Palette Map " + filename + " created successfully</color>");
+		} catch {
+			throw;
+		}
+	}
+	
+	public static Texture2D CreatePaletteMapFromSource(Texture2D sourceTexture)
+	{
+		Texture2D outTexture = new Texture2D (sourceTexture.width, sourceTexture.height, TextureFormat.Alpha8, false);
+		outTexture.hideFlags = HideFlags.HideAndDontSave;
+		
+		Color[] sourcePixels = sourceTexture.GetPixels ();
+		List<Color> uniqueColorsInSource = new List<Color> ();
+		
+		// Get all unique colors
+		for(int i = 0; i < sourcePixels.Length; i++) {
+			if(!uniqueColorsInSource.Contains(sourcePixels[i])) {
+				uniqueColorsInSource.Add (sourcePixels[i]);
+			}
+		}
+		
+		// Remap original colors to point to indeces in the palette
+		Color[] paletteMapPixels = new Color[sourcePixels.Length];
+		for(int i = 0; i < sourcePixels.Length; i++) {
+			int paletteIndex = uniqueColorsInSource.IndexOf(sourcePixels[i]);
+			float alpha;
+			if(uniqueColorsInSource.Count == 1) {
+				alpha = 0.0f;
+			} else {
+				alpha = paletteIndex / (float)(uniqueColorsInSource.Count - 1);
+			}
+			paletteMapPixels[i] = new Color(0.0f, 0.0f, 0.0f, alpha);
+		}
+		
+		outTexture.SetPixels(paletteMapPixels);
+		outTexture.Apply ();
+		
+		return outTexture;
+	}
+}
--- Assets/Editor/PaletteMapperWindow.cs
+++ Assets/Editor/PaletteMapperWindow.cs
@@ -1,7 +1,6 @@
 ﻿using UnityEngine;
 using UnityEditor;
 using System.Collections;
-using System.Collections.Generic;
 
 class PaletteMapperWindow : EditorWindow
 {
@@ -29,11 +28,13 @@ class PaletteMapperWindow : EditorWindow
 
 			try {
 				Texture2D inTexture = (Texture2D)inSourceTexture;
-				ValidateSourceTexture (inTexture);
+				PaletteMapper.ValidateSourceTexture (inTexture);
 				try {
 					string path = GetPathToAsset(inTexture);
 					try {
-						WritePaletteMapTextureToDisk (path, inTexture);
+						PaletteMapper.WritePaletteMapTextureToDisk (path, inTexture, overwriteExistingFiles);
+						
+						Debug.Log ("<color=green>Palette Map and Key for file " + inTexture.name + " created successfully</color>");
 					} catch (System.Exception e) {
 						Debug.LogError("PaletteMap Error: Encountered file error when trying to write PaletteMap: " 
 						               + e.Message);
@@ -47,22 +48,6 @@ class PaletteMapperWindow : EditorWindow
 		}
 	}
 
-	void ValidateSourceTexture (Texture2D sourceTexture)
-	{
-		TextureImporter textureImporter = AssetImporter.GetAtPath(AssetDatabase.GetAssetPath(sourceTexture)) as TextureImporter;
-		if(!textureImporter.isReadable) {
-			throw new System.ArgumentException("PaletteMapper Error: Source texture must be Read/Write enabled.");
-		}
-
-		if (sourceTexture.filterMode != FilterMode.Point) {
-			throw new System.ArgumentException ("PaletteMapper Error: Source texture must have Point filter mode.");
-		}
-
-		if (sourceTexture.format != TextureFormat.RGBA32) {
-			throw new System.ArgumentException ("PaletteMapper Error: Source texture must be uncompressed (RGBA32)");
-		}
-	}
-
 	string GetPathToAsset(Object asset)
 	{
 		string path = AssetDatabase.GetAssetPath(asset);
@@ -72,81 +57,5 @@ class PaletteMapperWindow : EditorWindow
 		path = path.TrimEnd(directories[directories.Length -1].ToCharArray());
 
 		return path;
-	}
-
-	void WritePaletteMapTextureToDisk (string outputPath, Texture2D inTexture)
-	{
-		// Create full path to output file
-		string paletteMapSuffix = "_PaletteMap.png";
-		string filename = inTexture.name + paletteMapSuffix;
-		string fullPathToOutputFile = outputPath + filename;
-
-		// Handle file overwriting
-		if(System.IO.File.Exists(fullPathToOutputFile)) {
-			if(!overwriteExistingFiles) {
-				Debug.LogError("PaletteMap Error: Tried to write " + filename + " but file already exists. " +
-				               "\nFile Path: " + outputPath);
-				return;
-			}
-			Debug.LogWarning("PaletteMap: Overwriting file " + filename);
-		}
-
-		// Create the PaletteMap texture
-		Texture2D outTexture = CreatePaletteMapFromSource(inTexture);
-
-		// Write the PaletteMap to disk
-		try {
-			byte[] outTextureData = outTexture.EncodeToPNG ();
-			System.IO.File.WriteAllBytes (fullPathToOutputFile, outTextureData);
-
-			// Assign correct settings to the file
-			TextureImporter textureImporter = AssetImporter.GetAtPath(fullPathToOutputFile) as TextureImporter; 
-			textureImporter.filterMode = FilterMode.Point;
-			textureImporter.textureFormat = TextureImporterFormat.Alpha8;
-			textureImporter.alphaIsTransparency = true;
-			textureImporter.mipmapEnabled = false;
-			textureImporter.npotScale = TextureImporterNPOTScale.None;
-			
-			// Force Unity to see the file and use the new import settings
-			AssetDatabase.ImportAsset(fullPathToOutputFile); 
-
-			Debug.Log ("<color=green>Palette Map " + filename + " created successfully</color>");
-		} catch {
-			throw;
-		}
-	}
-
-	Texture2D CreatePaletteMapFromSource(Texture2D sourceTexture)
-	{
-		Texture2D outTexture = new Texture2D (sourceTexture.width, sourceTexture.height, TextureFormat.Alpha8, false);
-		outTexture.hideFlags = HideFlags.HideAndDontSave;
-
-		Color[] sourcePixels = sourceTexture.GetPixels ();
-		List<Color> uniqueColorsInSource = new List<Color> ();
-
-		// Get all unique colors
-		for(int i = 0; i < sourcePixels.Length; i++) {
-			if(!uniqueColorsInSource.Contains(sourcePixels[i])) {
-				uniqueColorsInSource.Add (sourcePixels[i]);
-			}
-		}
-
-		// Remap original colors to point to indeces in the palette
-		Color[] paletteMapPixels = new Color[sourcePixels.Length];
-		for(int i = 0; i < sourcePixels.Length; i++) {
-			int paletteIndex = uniqueColorsInSource.IndexOf(sourcePixels[i]);
-			float alpha;
-			if(uniqueColorsInSource.Count == 1) {
-				alpha = 0.0f;
-			} else {
-				alpha = paletteIndex / (float)(uniqueColorsInSource.Count - 1);
-			}
-			paletteMapPixels[i] = new Color(0.0f, 0.0f, 0.0f, alpha);
-		}
-
-		outTexture.SetPixels(paletteMapPixels);
-		outTexture.Apply ();
-
-		return outTexture;
 	}
 }
