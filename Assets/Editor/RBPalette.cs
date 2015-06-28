e42d2d221ec7f77910014fc888cce34b3c4eacfe
Edward Rowe <edward.l.rowe@gmail.com>
2015-06-28 19:37:18 -0400

    Added framework for RBPalette.

---
+++ Assets/Editor/RBPalette.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class RBPalette : ScriptableObject {
+
+	public List<RBPaletteEntry> ColorsInPalette = new List<RBPaletteEntry> ();
+
+	[System.Serializable]
+	public class RBPaletteEntry {
+		public Color color;
+		public string label;
+
+		public RBPaletteEntry (Color color, string label)
+		{
+			this.color = color;
+			this.label = label;
+		}
+	}
+}
---
+++ Assets/Editor/RBPaletteCreator.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public static class RBPaletteCreator {
+
+	[MenuItem ("Assets/Create/RBPalette")]
+	public static RBPalette CreatePalette ()
+	{
+		// TODO: Needs to support creating multiple at this location
+		return CreatePalette (GetPathOfSelection(), "RBPalette.asset");
+	}
+
+	static string GetPathOfSelection ()
+	{
+		// TODO THIS DOESN"T ALWAYS WORK.
+		string path = "Assets";
+		foreach (UnityEngine.Object obj in Selection.GetFiltered(typeof(UnityEngine.Object), SelectionMode.Assets))
+		{
+			path = AssetDatabase.GetAssetPath(obj);
+			if (File.Exists(path))
+			{
+				path = Path.GetDirectoryName(path);
+			}
+			break;
+		}
+
+		return path;
+	}
+	
+	static RBPalette CreatePalette (string path, string filename)
+	{
+		RBPalette paletteAsset = ScriptableObject.CreateInstance<RBPalette> ();
+
+		string fullpath = path + "/" + filename;
+		AssetDatabase.CreateAsset (paletteAsset, fullpath);
+		AssetDatabase.SaveAssets ();
+		
+		EditorUtility.FocusProjectWindow ();
+		Selection.activeObject = paletteAsset;
+		
+		return paletteAsset;
+	}
+}
