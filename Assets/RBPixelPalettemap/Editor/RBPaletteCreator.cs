56102843befb6982e30379722de75ece2da86b5d
Edward Rowe <edward.l.rowe@gmail.com>
2015-08-04 12:43:12 -0400

    Extract palette now names the file and warns for file overwriting.
    Generic Asset Creation method now appends a number on each file and won't overwrite files.

--- Assets/RBPixelPalettemap/Editor/RBPaletteCreator.cs
+++ Assets/RBPixelPalettemap/Editor/RBPaletteCreator.cs
@@ -4,11 +4,28 @@ using System.IO;
 
 public static class RBPaletteCreator {
 
+	const string suffix = "_PaletteGroup";
+
 	[MenuItem ("Assets/Create/RBPalette")]
 	public static RBPaletteGroup CreatePalette ()
 	{
-		// TODO: Needs to support creating duplicates (count) at this location - RBPalette0, RBPalette1 etc.
-		return CreatePalette (GetPathOfSelection(), "RBPaletteGroup.asset");
+		int fileAppendix = -1;
+		string unformattedFilename = "RBPaletteGroup{0}.asset";
+		string formattedFilename = string.Empty;
+		string path = GetPathOfSelection ();
+		for (int i = 0; i < 100; i++) {
+			formattedFilename = string.Format (unformattedFilename, i);
+			if (!File.Exists (path + "/" + formattedFilename)) {
+				fileAppendix = i;
+				break;
+			}
+		}
+		if (fileAppendix >= 0) {
+			return CreatePalette (GetPathOfSelection (), formattedFilename);
+		} else {
+			Debug.LogError ("Failed to create file. Too many generic RBPaletteGroups exist in save location.");
+			return null;
+		}
 	}
 
 	static RBPaletteGroup CreatePalette (string path, string filename)
@@ -50,15 +67,40 @@ public static class RBPaletteCreator {
 	public static RBPaletteGroup ExtractPalleteFromTexture ()
 	{
 		Texture2D selectedTexture = (Texture2D) Selection.activeObject;
-		RBPalette paletteFromTexture = RBPalette.CreatePaletteFromTexture (selectedTexture);
-		RBPaletteGroup paletteGroup = RBPaletteGroup.CreateInstance (paletteFromTexture);
-		return SaveRBPalette (paletteGroup, GetPathOfSelection (), "RBPalette.asset");
+		string selectionPath = GetPathOfSelection ();
+		string filename = selectedTexture.name + suffix + ".asset";
+
+		// Check for existing file and warn for overwrite
+		bool writeFile = false;
+		if (File.Exists (selectionPath + "/" + filename)) {
+			if (EditorUtility.DisplayDialog ("Warning!", 
+			                                "This will overwrite the existing file, " + filename + 
+			                                 ". Are you sure you want to extract the palette?", "Yes", "No")) {
+				writeFile = true;
+			}
+		} else {
+			writeFile = true;
+		}
+
+		// Extract and write the texture
+		if (writeFile) {
+			RBPalette paletteFromTexture = RBPalette.CreatePaletteFromTexture (selectedTexture);
+			RBPaletteGroup paletteGroup = RBPaletteGroup.CreateInstance (paletteFromTexture);
+			return SaveRBPalette (paletteGroup, selectionPath, filename);
+		}
+
+		return null;
 	}
 	
 	[MenuItem ("Assets/ExtractPalette", true)]
 	public static bool IsValidTargetForPalette ()
 	{
 		if (Selection.activeObject == null) {
+			return false;
+		}
+
+		// TODO: Support multi-texture palette group extraction
+		if (Selection.objects.Length > 1) {
 			return false;
 		}
 
