a05a177e397333ae26aa9c3afd96da80628a4fdb
Edward Rowe <edward.l.rowe@gmail.com>
2015-08-04 13:46:44 -0400

    Added PackageExporter script. Exported updated package.

---
+++ Assets/Editor/PackageExporter.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class PackageExporter : UnityEditor.EditorWindow {
+
+	static string assetPathName = "Assets/RBPixelPalettemap";
+
+	[MenuItem ("PackageExporter/Export as Package")]
+	public static void ExportPRBScripts ()
+	{
+		AssetDatabase.ExportPackage (assetPathName, "RBPixelPalettemap.unitypackage", ExportPackageOptions.Recurse |
+		                             ExportPackageOptions.IncludeDependencies);
+		Debug.Log ("Exported!");
+	}
+}
