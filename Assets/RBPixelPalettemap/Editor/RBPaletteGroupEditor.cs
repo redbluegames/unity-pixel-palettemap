82b7b2607b58671651e637a0bf6348a62a5297a3
Edward Rowe <edward.l.rowe@gmail.com>
2015-09-02 19:58:02 -0400

    Cleaned up code a bit and improved aesthetic of palettes in Palette View.

--- Assets/RBPixelPalettemap/Editor/RBPaletteGroupEditor.cs
+++ Assets/RBPixelPalettemap/Editor/RBPaletteGroupEditor.cs
@@ -11,7 +11,7 @@ public class RBPaletteGroupEditor : Editor {
 
 	bool isListLocked = false;
 	#region Sizing
-	float colorPaddingX = 5.0f;
+	float colorPaddingX = 10.0f;
 	float colorSpacing = 5.0f;
 	float palettePadding = 5.0f;
 	float colorWidth = 50.0f;
@@ -30,9 +30,12 @@ public class RBPaletteGroupEditor : Editor {
 			EditorGUI.LabelField(rect, ((RBPaletteGroup)target).GroupName + " - Palette Mode");
 		};
 		list.drawElementCallback = DrawListElement;
+		list.drawElementBackgroundCallback = DrawListElementBackground; 
 
 		// TODO: Don't let them reorder Base Palette
 	}
+	
+	#region Reorderable Callbacks
 
 	bool CanRemovePaletteFromList (ReorderableList list) 
 	{
@@ -40,34 +43,46 @@ public class RBPaletteGroupEditor : Editor {
 		if (list.index == 0) {
 			return false;
 		}
-
+		
 		return !isListLocked;
 	}
-
+	
 	float GetElementHeight ()
 	{
+		// Get number of lines we need to show all colors given the width
 		RBPaletteGroup targetRBPaletteGroup = (RBPaletteGroup) target;
-		int numlines = Mathf.CeilToInt (targetRBPaletteGroup.NumColorsInPalette / (float) GetMaxNumColorsPerLine ());
+		int numColorsInPalette = targetRBPaletteGroup.NumColorsInPalette;
+		int numColorLines = Mathf.CeilToInt (numColorsInPalette / (float) GetMaxNumColorsPerLine ());
+
+		// Get size we will need to show all lines
 		float sizePerLine = EditorGUIUtility.singleLineHeight + palettePadding;
-		return numlines * sizePerLine;
+		float expectedSize = numColorLines * sizePerLine;
+		// At minimum show a line per palette
+		expectedSize = Mathf.Max (expectedSize, sizePerLine) + palettePadding;
+
+		return expectedSize;
 	}
 	
 	int GetMaxNumColorsPerLine ()
 	{
 		// w = (n-1) * s + N * c + 2p  [solve for n = (w - 2p + s) / (s + c)]
 		// Where n = numColors, s = spacing, c = colorwidth, p = padding, w = availableWidth
+		float effectiveColorWidth = colorWidth;
 		float windowWidth = Screen.width;
+		float reorderableListDefaultPadding = 5.0f;
+		windowWidth -= reorderableListDefaultPadding;
 		float handleWidth = list.draggable ? 25.0f : 0.0f;
-		float availableWidth = windowWidth - labelWidth - handleWidth - (2 * colorPaddingX);
-		int numColors = Mathf.FloorToInt ((availableWidth - (2 * colorPaddingX) + colorSpacing) / (colorSpacing + colorWidth));
-
+		float availableWidth = windowWidth - labelWidth - handleWidth;
+		int numColors = Mathf.FloorToInt ((availableWidth - (2 * colorPaddingX) + colorSpacing) /
+		                                  (colorSpacing + effectiveColorWidth));
+		
 		return numColors;
 	}
 	
 	void DrawListElement (Rect rect, int index, bool isActive, bool isFocused)
 	{
 		var element = list.serializedProperty.GetArrayElementAtIndex(index);
-		rect.y += 2;
+		rect.y += palettePadding;
 		
 		// Draw label
 		SerializedProperty paletteName = element.FindPropertyRelative ("PaletteName");
@@ -78,7 +93,8 @@ public class RBPaletteGroupEditor : Editor {
 		} else {
 			EditorGUI.LabelField (labelRect, paletteName.stringValue);
 		}
-		
+
+		// Draw colors
 		SerializedProperty colorsAsList = element.FindPropertyRelative ("ColorsInPalette");
 		int numColorsPerLine = GetMaxNumColorsPerLine ();
 		List<SerializedProperty> colorProperties = GetListFromSerializedProperty (colorsAsList);
@@ -93,6 +109,32 @@ public class RBPaletteGroupEditor : Editor {
 			//EditorGUIUtility.DrawColorSwatch (colorRect, colorProperties[i].colorValue);
 		}
 	}
+
+	void DrawListElementBackground (Rect rect, int index, bool isActive, bool isFocused)
+	{
+		// Draws a bar on top and bottom to give depth effect,
+		// and box background to prevent element overlap while dragging.
+		float barHeight = 1.0f;
+		rect.width -= 3.0f; // Subtract and shift or it hangs over edges
+		rect.x += 1.0f;
+		
+		Rect topRect = new Rect (rect.x, rect.y, rect.width, barHeight);
+		EditorGUI.DrawRect (topRect, Color.gray);
+		
+		Rect bottomRect = new Rect (rect.x, rect.y + rect.height - barHeight, rect.width, barHeight);
+		EditorGUI.DrawRect (bottomRect, new Color (0.2f, 0.2f, 0.2f));
+		
+		Vector2 padding = new Vector2 (1.0f, 1.0f);
+		Rect backgroundRect = new Rect (rect.x + padding.x, rect.y + padding.y, 
+			rect.width - (2 * padding.x), rect.height - (2 * padding.y));
+
+		// Background colors approximate editor skin. This would need to change if
+		// editor colors change.
+		Color backgroundColor = EditorGUIUtility.isProSkin ? new Color (0.3f, 0.3f, 0.3f) :
+			new Color (0.9f, 0.9f, 0.9f);
+		EditorGUI.DrawRect (backgroundRect, backgroundColor);
+	}
+	#endregion
 
 	void SetGroupLocked (bool locked)
 	{
@@ -131,28 +173,12 @@ public class RBPaletteGroupEditor : Editor {
 			targetRBPaletteGroup.RemoveColorAtIndex (colorIndex);
 		}
 
-		// TODO: Remove Palette Editing once it works through + / - buttons
-		/*
-		EditorGUILayout.Space ();
-		EditorGUILayout.LabelField ("Palettes", EditorStyles.boldLabel);
-		
-		if( GUILayout.Button( "Add Palette", GUILayout.ExpandWidth(false)) )
-		{
-			targetRBPaletteGroup.AddPalette ();
-		}
-		
-		paletteIndex = EditorGUILayout.IntSlider ("Palette index: ", paletteIndex, 0, targetRBPaletteGroup.Count - 1);
-		if( GUILayout.Button( "Remove Palette At Index", GUILayout.ExpandWidth(false)) )
-		{
-			targetRBPaletteGroup.RemovePaletteAtIndex (paletteIndex);
-		}
-		*/
 		EditorGUILayout.Space ();
 		EditorGUILayout.LabelField ("Utilities", EditorStyles.boldLabel);
 
 		if( GUILayout.Button( "Export As Texture", GUILayout.ExpandWidth(false)) )
 		{
-			string outputPath = GetPathToAsset (targetRBPaletteGroup);
+			string outputPath = AssetDatabaseUtility.GetAssetDirectory (targetRBPaletteGroup);
 			string extension = ".png";
 			string filename = targetRBPaletteGroup.GroupName + extension;
 			try {
@@ -167,17 +193,6 @@ public class RBPaletteGroupEditor : Editor {
 		}
 
 		serializedObject.ApplyModifiedProperties();
-	}
-	
-	string GetPathToAsset (Object asset)
-	{
-		string path = AssetDatabase.GetAssetPath (asset);
-		
-		// Strip filename out from asset path
-		string[] directories = path.Split ('/');
-		path = path.TrimEnd (directories [directories.Length - 1].ToCharArray ());
-		
-		return path;
 	}
 	
 	/// <summary>
